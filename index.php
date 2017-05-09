26e5832af2d9ba00e127c0ccbc20aad059bbe328
Unknown <Jeppe_Skjerk_Bjerrum@hotmail.com>
2017-05-09 13:29:21 +0200

    skabelon til forsiden er done!

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Untitled Document</title>
+    <title>løve's incorporated</title>
     <!-- Bootstrap -->
-	<link href="file:///C|/xampp/htdocs/jeppes_site/css/bootstrap.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/stylesheet.css">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,11 +17,18 @@
 		<![endif]-->
   </head>
   <body>
-  <h1>Hello world</h1>
+  <div class="container-fluid">
+  	<div class="row">
+		<div class="col-md-6 col-xs-hidden bogcafelink"><a href="bogcafe.php"><img src="images/bogcafe1.jpg" alt=""></a></div>
+  		<div class="col-md-6 col-xs-hidden vincafelink"><a href="vincafe.php"><img src="images/vincafe1.jpg" alt=""></a></div>
+  		
+  	
+  </div>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-	<script src="file:///C|/xampp/htdocs/jeppes_site/js/jquery-1.11.3.min.js"></script>
-
+	<script src="js/jquery-1.11.3.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed --> 
-	<script src="file:///C|/xampp/htdocs/jeppes_site/js/bootstrap.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/script.js"
+	></script>
   </body>
 </html>
