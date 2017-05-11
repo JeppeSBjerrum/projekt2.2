d6fb4ca549fc58fba21632f28d180a8488b260ae
Johanne <johannejungild@gmail.com>
2017-05-11 11:01:51 +0200

    lorte aqua baggrund

---
+++ footer.php
@@ -0,0 +1,70 @@
+<footer>
+<img src="images/mammismaps.JPG" alt="">
+<div class="container">
+<div class="row">
+<div class="col-sm-3">
+	<h3>Bogcaféen</h3>
+	<p>Løve's Bog- og VinCafé<br>
+
+Nørregade 32<br>
+
+8000 Århus C<br>
+<br>
+<br>
+
+
+27 83 16 33<br>
+
+aa-cafe@loeves.dk<br>
+<br>
+
+	Åbningstider <br>
+
+Mandag-Fredag: 9-24 <br>
+
+Lørdag: 10-24 <br>
+
+Søndag: 10-18 </p>
+</div>
+<div class="col-sm-3">
+	<h3>Vincaféen</h3>
+	<p>
+		Vincaféen<br>
+
+Borggade 14<br>
+
+8000 Århus C
+<br>
+<br>
+
+52 17 50 16<br>
+
+vincafeen@loeves.dk<br>
+<br>
+Åbningstider <br>
+
+Mandag: Lukket <br>
+
+Tirsdag: 16-22 <br>
+
+Onsdag: 16-22 <br>
+
+Torsdag: 16-24 <br>
+
+Fredag: 15-24 <br>
+
+Lørdag: 15-24 <br>
+
+Søndag: Lukket 
+	</p>
+</div>
+	
+</div>
+</div>
+</footer>
+</div> <!--container-fluid afsluttet--> 
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	
+  </body>
+</html>
--- vincafe.php
+++ vincafe.php
@@ -1,20 +1,18 @@
+
 <!DOCTYPE html>
 <html id="vinsite" lang="da">
   <head>
+   <!--metatags-->
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+   <!--title-->
     <title>Vincaféen</title>
-    <!-- Bootstrap -->
+    <!-- Bootstrap og stylesheet -->
 	<link href="css/bootstrap.css" rel="stylesheet">
 	<link rel="stylesheet" href="css/stylesheet.css">
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+	<!--icon-->
+	
   </head>
   <body>
   <div class="container-fluid">
@@ -30,8 +28,9 @@
   <!--Stor video-->
   <div>
   	
-  </div></div>
+  </div>
   <!--Velkomsttekst-->
+  <div id="vinvelkommenid">
   <div class="container">
   	<h1>Velkommen til Løve's Vincafé</h1>
   	<p>Åbningstider<br>
@@ -49,8 +48,9 @@ Fredag: 15-24 <br>
 Lørdag: 15-24 <br>
 
 Søndag: Lukket </p>
-  </div>
-  <div class=container-fluid>
+  </div></div>
+  <!--CAROUSEL-->
+  
  <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
@@ -63,8 +63,8 @@ Søndag: Lukket </p>
     <div class="carousel-inner">
 
       <div class="item active">
-        <img src="images/IMG_0760.JPG" alt="">
-        <div class="carousel-caption">
+        <img src="images/carouselbillede1.JPG" alt="">
+        <div class="carousel-caption boks">
           <h3>Løve’s Bogklub</h3>
           <p>
 
@@ -85,16 +85,16 @@ Emil Bønnelycke <br>
       </div>
 
       <div class="item">
-        <img src="images/IMG_0760.JPG" alt="">
-        <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago!</p>
+        <img src="images/carouselbillede1.JPG" alt="">
+        <div class="carousel-caption boks">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
         </div>
       </div>
     
       <div class="item">
-        <img src="images/IMG_0760.JPG" alt="">
-        <div class="carousel-caption">
+        <img src="images/carouselbillede1.JPG" alt="">
+        <div class="carousel-caption boks">
           <h3>New York</h3>
           <p>We love the Big Apple!</p>
         </div>
@@ -112,12 +112,64 @@ Emil Bønnelycke <br>
       <span class="sr-only">Next</span>
     </a>
   </div>
+	  
+	  <!--MENU-->
+	  <div id="menuid">
+	  <div class="container">
+	  	<div class="row">
+	  	<h2>Menu</h2>
+	  		<div class="col-sm-6">
+	  			<!--CAROUSEL-->
+ <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="images/IMG_0760.JPG" alt="">
+        <div class="carousel-caption boks">
+          <h3>Løve’s Bogklub</h3>
+          <p>En beksrivelse</p>
+        </div>
+      </div>
+
+	  		</div>
+				</div>
+			</div>
+  		
+	  		<div class="col-sm-6">
+	  		<div class="row">
+	  			<h4 class="col-xs-6 col-sm-3">Vinkort</h4>
+	  			<h4 class="col-xs-6 col-sm-3">Varmt</h4>
+	  			<h4 class="col-xs-6 col-sm-3">Koldt</h4>
+	  			<h4 class="col-xs-6 col-sm-3">Mad</h4>
+	  		</div>
+				<p id="menuindhold">Her skal der komme js</p>
+	  		</div>
+	  	</div>
+	  	
+	  </div></div>
+	  <div id="smagningid">
+	  <div class="container">
+	  <div class="row">
+	  	<div class="col-sm-6 boks"><h2>Privat vinsmagning</h2>
+	  	<p>Hvis du mangler et sted at holde din fest, fødselsdag, reception, polterabend, konfirmation eller andet selskabelighed, så har vi lokalerne til dig. I hyggelige og rustikke omgivelser tilbyder vi ikke blot et lokale med plads til mere end 30 personer, men også en velrustet bar, et godt køkken og ikke mindst søde vintjenere. Om det er på en hverdag eller i weekenden, du ønsker at booke lokalet, betyder ikke noget. Du skriver bare en mail til aa-cafe@loeves.dk med dine ønsker og så stykker vi din begivenhed sammen efter dine behov.<br>
+<br>
+
+Lokalerne kan bookes fra mandag til fredag i tidsrummet 9-24, lørdag 10-24 samt om søndagen fra 10-18.
+</p>
+ <a href="#"> <p class="btn-default btn">Kontakt os</p></a>
+  </div>
 	  </div>
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-	<script src="js/jquery-1.11.3.min.js"></script>
-
-	<!-- Include all compiled plugins (below), or include individual files as needed --> 
-	<script src="js/bootstrap.js"></script>
-  </body>
-</html>
+	  	
+	  </div>
+</div>
+	  
+	  
+	  <?php include('footer.php') ?>
