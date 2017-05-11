58e88163e3dc23cb12970553a10f8b6ef0b2c247
Johanne <johannejungild@gmail.com>
2017-05-11 09:29:37 +0200

    Henriettas kaffe

--- vincafe.php
+++ vincafe.php
@@ -1,10 +1,123 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<!DOCTYPE html>
+<html lang="da">
+  <head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vincaféen</title>
+    <!-- Bootstrap -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/stylesheet.css">
 
-<body>
-</body>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+  </head>
+  <body>
+  <div class="container-fluid">
+  <!--Logo og nav-->
+  <img src="images/Løveslogostempel.png" alt="">
+  <nav>
+  <ul>
+  	<li><a href="bogcafe.php">Bogcafé</a></li>
+  	<li><a href="vincafe.php">Vincafé</a></li>
+  	<li><a href="kontakt.php">Kontakt</a></li>
+  </ul>
+  </nav>
+  <!--Stor video-->
+  <div>
+  	
+  </div></div>
+  <!--Velkomsttekst-->
+  <div class="container">
+  	<h1>Velkommen til Løve's Vincafé</h1>
+  	<p>Åbningstider<br>
+
+  	Mandag: Lukket <br>
+
+Tirsdag: 16-22 <br>
+
+Onsdag: 16-22 <br>
+
+Torsdag: 16-24 <br>
+
+Fredag: 15-24 <br>
+
+Lørdag: 15-24 <br>
+
+Søndag: Lukket </p>
+  </div>
+  <div class=container-fluid>
+ <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="images/IMG_0760.JPG" alt="">
+        <div class="carousel-caption">
+          <h3>Løve’s Bogklub</h3>
+          <p>
+
+Løve's BogKlub er et uforpligtende forum, hvor vi mødes den 2. tirsdag i hver måned klokken 19 og snakker om en udvalgt bog. Formålet er at samles om litteratur og vende nogle af de tanker, bøger skaber under og efter læsningen.
+
+Valget af bøger foretages i fællesskab og kan købes i bogcafeen. Du kan tilmelde dig til BogKlubben ved at skrive en mail til aa-cafe@loeves.dk med dit navn og emailadresse eller komme ned i bogcafeen og blive skrevet op. Og som sagt er det ganske uforpligtende at være med. Du kan bare troppe op til de møder, du syntes lyder interessante.
+<br>
+<br>
+
+Denne uge: <br>
+
+Bog #75 <br>
+
+Emil Bønnelycke <br>
+
+"Spartanerne"</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="images/IMG_0760.JPG" alt="">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago!</p>
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="images/IMG_0760.JPG" alt="">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+	  </div>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.3.min.js"></script>
+
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.js"></script>
+  </body>
 </html>
